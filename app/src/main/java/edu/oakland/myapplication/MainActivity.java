4d0d6124052e52a372faa1724df518d317dc7746
Matthew Fair <mfair@oakland.edu>
2017-11-13 17:15:40 -0500

    Updated Functionality for Search Function

--- app/src/main/java/edu/oakland/myapplication/MainActivity.java
+++ app/src/main/java/edu/oakland/myapplication/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends Activity implements
         final String searchVariable = trackSearch.getText().toString();
         searchButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                SearchTrackString("T-Pain Mashup");
+                SearchTrackString("T-Pain");
             }
         });
 
